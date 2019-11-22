aba64468691f169f002ddb4db818ca43b6e44592
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-22 15:07:17 -0300

    Projeto criado

---
+++ src/main/java/com/lucasms/projforum/ProjforumApplication.java
@@ -0,0 +1,13 @@
+package com.lucasms.projforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasms/projforum/ProjforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasms.projforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
