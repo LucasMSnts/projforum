d033e3134ff0f57c9e46c2512e99e3e7122bfb46
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-22 22:50:47 -0300

    Obtendo um usuario por id

--- src/main/java/com/lucasms/projforum/services/UserService.java
+++ src/main/java/com/lucasms/projforum/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasms.projforum.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasms.projforum.domain.User;
 import com.lucasms.projforum.repository.UserRepository;
+import com.lucasms.projforum.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
