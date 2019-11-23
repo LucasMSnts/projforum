d718cc788de3a697ba2d3e0d18e71f44fe145fe2
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-23 13:21:48 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/lucasms/projforum/config/Instantiation.java
+++ src/main/java/com/lucasms/projforum/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lucasms.projforum.domain.Post;
 import com.lucasms.projforum.domain.User;
 import com.lucasms.projforum.dto.AuthorDTO;
+import com.lucasms.projforum.dto.CommentDTO;
 import com.lucasms.projforum.repository.PostRepository;
 import com.lucasms.projforum.repository.UserRepository;
 
@@ -40,7 +41,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/12/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/12/2019"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/12/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/12/2019"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/12/2019"), new AuthorDTO(alex));
 
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
