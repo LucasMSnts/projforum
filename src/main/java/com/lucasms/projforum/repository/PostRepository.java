0fd9e9eab6becce512bbcf2971e5a51703f0cc7a
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-23 14:01:07 -0300

    Consulta simples com @Query

--- src/main/java/com/lucasms/projforum/repository/PostRepository.java
+++ src/main/java/com/lucasms/projforum/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucasms.projforum.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucasms.projforum.domain.Post;
@@ -10,5 +11,10 @@ import com.lucasms.projforum.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	// https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
+	// https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#repositories.query-methods
 	List<Post> findByTitleContainingIgnoreCase(String text);	
 }
